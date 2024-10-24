70ee53f21926e0cc51b3f3553451df299f0d0868
rachneum <rachel-1604@hotmail.com>
2024-10-24 11:45:33 +0200

    test

--- to_do.c
+++ to_do.c
@@ -12,3 +12,17 @@ TO_DO_LIST
 - check parsing for built-ins
 
 [[EXECUTION PART]]
+
+/*
+>   out_red Redirige la sortie (stdout) d'une commande vers un fichier. Si le fichier existe déjà, il est écrasé.
+ex: echo "Bonjour" > fichier.txt
+La commande echo "Bonjour" va afficher "Bonjour" dans le terminal. Avec >, la sortie est envoyée dans le fichier fichier.txt. Si le fichier existe déjà, son contenu sera remplacé par "Bonjour".
+
+>>  Redirige la sortie (stdout) d'une commande vers un fichier, mais cette fois-ci, au lieu de l'écraser, il ajoute la sortie à la fin du fichier.
+ex: echo "Salut" >> fichier.txt
+La commande ajoute "Salut" à la fin du fichier fichier.txt sans supprimer son contenu précédent.
+
+<   in_red  Prend l'entrée d'une commande à partir d'un fichier au lieu de l'attendre dans le terminal.
+ex: cat < fichier.txt
+la commande cat lit le contenu de fichier.txt grâce à < et l'affiche dans le terminal.
+*/
