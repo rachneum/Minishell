3a951535384601213093d06aba64aadf2e2125f8
rachneum <rachel-1604@hotmail.com>
2024-11-22 16:58:21 +0100

    test

--- built_in/built_in.c
+++ built_in/built_in.c
@@ -28,25 +28,25 @@ int	my_pwd(t_cmd *cmd)
 	return (0);
 }
 
-/*void	my_echo(char **arg)
+void	my_echo(char **arg)
 {
 	int	i;
 	int	flag;
 
 	i = 1;
 	flag = 0;
-	if (arg[i] == ft_strncmp(arg[i], "-n", 2))
+	if (!ft_strncmp(arg[i], "-n", 2))
 		flag = 1;
 	while (arg[i + flag] != NULL)
 	{
-		printf("%s", arg[i]);
+		printf("%s", arg[i + flag]);
 		i++;
 		if (arg[i + flag] != NULL)
-			printf("' '");
+			printf(" ");
 	}
 	if (!flag)
 		printf("\n");
-}*/
+}
 
 void	my_env(t_cmd *cmd, t_all *all)
 {
